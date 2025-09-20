53a11a3226927d38f79b60d5d32e244cc2366ac0
Daniel Camuçatto <daniel.camucatto@gmail.com>
2025-09-20 15:56:10 -0300

    feat: adiciona tootltips, links e icones para redes sociais

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,20 @@
 
 import { getTranslations } from '../utils/translations';
 
+interface SocialLink {
+  url: string;
+  label: string;
+}
+
 interface HeaderTranslations {
   name: string;
   role: string;
   description: string;
+  socialLinks: {
+    linkedin: SocialLink;
+    github: SocialLink;
+    devto: SocialLink;
+  };
 }
 
 export default function Header() {
@@ -24,6 +34,57 @@ export default function Header() {
             {translations.description}
           </p>
         </div>
+
+        <ul className="ml-1 mt-8 flex items-center">
+          <li className="mr-5 relative group">
+            <a
+              className="block hover:text-teal-300"
+              href={translations.socialLinks.linkedin.url}
+              target="_blank"
+              rel="noreferrer"
+              aria-label="LinkedIn"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
+                <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+              </svg>
+            </a>
+            <span className="absolute -top-12 left-1/2 -translate-x-1/2 opacity-0 transition-opacity group-hover:opacity-100 bg-slate-800 text-slate-200 px-3 py-1 text-sm rounded whitespace-nowrap pointer-events-none">
+              Conexões Profissionais
+            </span>
+          </li>
+          <li className="mr-5 relative group">
+            <a
+              className="block hover:text-teal-300"
+              href={translations.socialLinks.github.url}
+              target="_blank"
+              rel="noreferrer"
+              aria-label="GitHub"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
+                <path d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.17 6.839 9.49.5.092.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.603-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.464-1.11-1.464-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.578 9.578 0 0112 6.836c.85.004 1.705.114 2.504.336 1.909-1.294 2.748-1.025 2.748-1.025.546 1.377.202 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.167 22 16.418 22 12c0-5.523-4.477-10-10-10z"></path>
+              </svg>
+            </a>
+            <span className="absolute -top-12 left-1/2 -translate-x-1/2 opacity-0 transition-opacity group-hover:opacity-100 bg-slate-800 text-slate-200 px-3 py-1 text-sm rounded whitespace-nowrap pointer-events-none">
+              Veja meus códigos
+            </span>
+          </li>
+          <li className="mr-5 relative group">
+            <a
+              className="block hover:text-teal-300"
+              href={translations.socialLinks.devto.url}
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Dev.to"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
+                <path d="M7.42 10.05c-.18-.16-.46-.23-.84-.23H6l.02 2.44.04 2.45.56-.02c.41 0 .63-.07.83-.26.24-.24.26-.36.26-2.2 0-1.91-.02-1.96-.29-2.18zM0 4.94v14.12h24V4.94H0zM8.56 15.3c-.44.58-1.06.77-2.53.77H4.71V8.53h1.4c1.67 0 2.16.18 2.6.9.27.43.29.6.32 2.57.05 2.23-.02 2.73-.47 3.3zm5.09-5.47h-2.47v1.77h1.52v1.28l-.72.04-.75.03v1.77l1.22.03 1.2.04v1.28h-1.6c-1.53 0-1.6-.01-1.87-.3l-.3-.28v-3.16c0-3.02.01-3.18.25-3.48.23-.31.25-.31 1.88-.31h1.64v1.3zm4.68 5.45c-.17.43-.64.79-1 .79-.18 0-.45-.15-.67-.39-.32-.32-.45-.63-.82-2.08l-.9-3.39-.45-1.67h.76c.4 0 .75.02.75.05 0 .06 1.16 4.54 1.26 4.83.04.15.32-.7.73-2.3l.66-2.52.74-.04c.4-.02.73 0 .73.04 0 .14-1.67 6.38-1.8 6.68z"></path>
+              </svg>
+            </a>
+            <span className="absolute -top-12 left-1/2 -translate-x-1/2 opacity-0 transition-opacity group-hover:opacity-100 bg-slate-800 text-slate-200 px-3 py-1 text-sm rounded whitespace-nowrap pointer-events-none">
+              Leia meus artigos técnicos
+            </span>
+          </li>
+        </ul>
       </div>
     </header>
   );
